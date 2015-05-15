7db597ade83547f76afce8be8e72fbbab6ee5da3
Justin Huntington <corusco@gmail.com>
2015-05-15 16:17:35 -0600

    Code through step 1. Runs. White screen with nav.

---
+++ ListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListViewController.h
+//  Time-Tracker
+//
+//  Created by Justin Huntington on 5/15/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListViewController : UIViewController
+
+@end
--- Time-Tracker/TTAppDelegate.m
+++ Time-Tracker/TTAppDelegate.m
@@ -7,13 +7,16 @@
 //
 
 #import "TTAppDelegate.h"
+#import "ListViewController.h"
 
 @implementation TTAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[ListViewController new]];
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
